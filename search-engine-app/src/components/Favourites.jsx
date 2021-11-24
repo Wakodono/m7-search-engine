e4323e3c0fc8f7230495b9435c9df3cf4819feb1
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-11-24 15:52:30 +0000

    favourites component
    Wrapped App in provider to allow all components to access redux store

---
+++ search-engine-app/src/components/Favourites.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { FaTrash } from 'react-icons/fa'
+import { removeJobsAction } from '../actions'
+
+export const Favourites = ({ jobs, removeListings }) => {
+    return (
+        <Row>
+            <Col>
+            <ul style={{ listStyle: "none" }}>
+        {cart.map((listing, i) => (
+          <li key={i} className="my-4">
+            <Button variant="danger" onClick={() => removeListings(i)}>
+              <FaTrash />
+            </Button>
+            {listing.title}
+          </li>
+        ))}
+      </ul>
+            </Col>
+        </Row>
+    )
+}
+
+const mapStateToProps = (state) => ({
+    jobs: state.jobs.listings
+})
+
+const mapDispatchToProps = dispatch => ({
+  removeListing: (indexToRemove) => {
+      dispatch(removeFromJobsAction(index.ToRemove))
+  }  
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Favourites)
